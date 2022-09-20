6cbc0f42c57a844179ef82a5934de88c129903c1
luanc <luanc@LUAN>
2022-09-20 16:02:26 -0300

    Adicionar arquivos de projeto.

---
+++ CrudWeb/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using CrudWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace CrudWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+        #region requisições
+        public bool createProduct(ProductModel model)
+        {
+            var client = new RestClient("https://localhost:7299/");
+            var request = new RestRequest("Api/Create-Product", Method.Post);
+            request.AddBody(model);
+            return client.Execute<bool>(request).Data;
+        }
+        public IEnumerable<ProductModel> ListProduct()
+        {
+            var client = new RestClient("https://localhost:7299/");
+            var request = new RestRequest("Api/List-Products", Method.Get);
+            return client.Execute<List<ProductModel>>(request).Data;
+
+        }
+
+        public bool DeleteProduct(long id)
+        {
+            var client = new RestClient("https://localhost:7299");
+            var request = new RestRequest("Api/Delete-Product", Method.Delete);
+            request.AddParameter("id", id);
+            return client.Execute<bool>(request).Data;
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            ViewBag.Product = ListProduct();
+            return View();
+        }
+    }
+}
---
+++ CrudWeb/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudWeb.Models
+{
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
